d6dd485407bea4ebbdf64c87042eb636f9004d09
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-23 22:05:46 +0530

    variables

---
+++ variables_and_data_types.js
@@ -0,0 +1,11 @@
+var variable = 'variable';
+// ES6
+let letVar = 'let variable';
+const constVAr = 'const variable';
+let $name, _name, name; // $, _, any alphabet is valid first letter for a variable
+
+// data types:
+// primitive - Strings, Numbers, Boolean, Null, Undefined
+// complex - Object, Symbol
+
+console.log(constVAr);
