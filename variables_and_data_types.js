3d42d148d5cd5cdbb23481fb3f87a6bf203411a7
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-24 00:31:52 +0530

    primitive data types

--- variables_and_data_types.js
+++ variables_and_data_types.js
@@ -6,11 +6,39 @@ let letVar = 'let variable';
 const constVAr = 'const variable';
 
 // $, _, any alphabet is valid first letter for a variable
-let $name, _name, name;
+// let $name, _name, name;
 
 /* data types:
 primitive - Strings, Numbers, Boolean, Null, Undefined
 complex - Object, Symbol 
 */
 
-console.log(constVAr);
+// String
+// no difference in single and double quotes
+// static value
+const singleQuotes = 'single';
+const doubleQuotes = 'double';
+
+// backticks - for dynamic value
+const userName = 'john';
+const backticks = `Hello ${userName}`;
+const sum = `${17 + 17}`; // dynamic value
+
+// Numbers
+const wholeNumber = 17;
+const decimalNumber = 17.5;
+const string = 'Hello';
+// can multiply divide add subtract numbers and many other operations
+// wholeNumber/ decimalNumber -> Number
+// string / wholeNumber -> NaN -> typeof NaN -> number
+
+// Booleans -> true or false
+const answer = true; //or false
+
+// Null -> unknown or null or empty value
+const nothing = null; //typeof null is object
+
+// Undefined -> variable is declared but not assigned a value
+let x;
+
+console.log(typeof null);
