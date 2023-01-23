11c9b80899a46f000e8f0b9f94d1a654fd5b062e
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-23 22:13:36 +0530

    comments

--- variables_and_data_types.js
+++ variables_and_data_types.js
@@ -1,11 +1,16 @@
+// create a variable using var keyword
 var variable = 'variable';
-// ES6
+
+// // create a variable using let, const keyword (ES6)
 let letVar = 'let variable';
 const constVAr = 'const variable';
-let $name, _name, name; // $, _, any alphabet is valid first letter for a variable
 
-// data types:
-// primitive - Strings, Numbers, Boolean, Null, Undefined
-// complex - Object, Symbol
+// $, _, any alphabet is valid first letter for a variable
+let $name, _name, name;
+
+/* data types:
+primitive - Strings, Numbers, Boolean, Null, Undefined
+complex - Object, Symbol 
+*/
 
 console.log(constVAr);
