3d19206f1d4d96ffe32a2d0154e49e84af1310fd
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-24 18:48:20 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,52 @@
+/*
+Operators -> 
+    arithmetic 
+    comparison 
+    logical
+    assignment
+    ternary
+*/
+
+/* 
+Arithmetic operators
+    Addition -> a + b
+
+    Subtraction -> a - b
+    
+    Multiplication -> a * b
+    
+    Division -> a / b
+    
+    Exponent -> a ** b -> 2 ** 4 = 16
+    
+    Modulo -> a % b -> 4 % 3 = 1 (4 divide by 3 - remainder is 1) | 4 % 10 = 4 (remainder is 4 because we were not successful in dividing 4 by 10)
+    
+    Increment and Decrement -> increase or decrease a value by 1 -> Let a = 5, a++ gives a = 6, a-- gives a = 5 because previous value of a was 6.
+*/
+
+/*
+Comparison operators and equality
+    returns true or false
+    const a = 5, b = 10
+    a > b -> false
+    a < b -> true
+    10 < 10 -> false
+    10 <= 10 -> true
+    10 == 10 -> true
+    5 != 10 -> true -> 5 is not equal to 10
+
+    Strict equality
+    a === b
+    Strict inequality
+    a !== b
+
+    5 == "5" -> true - loose equality
+    5 === "5" -> false -> strict equality
+
+    Strict equality === -> compares values and data types and return true on both true
+
+    Loose equality == -> compares only values, doesn't compare data types
+
+    The good ones : === !==
+    The bad ones : == != (avoid using)
+*/
