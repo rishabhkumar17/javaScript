19ddd07e34ec605f3fe5d5aeac8d606740e153d4
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-26 17:15:04 +0530

    objects intro

---
+++ objects.js
@@ -0,0 +1,15 @@
+// Objects
+// is an unordered colletion of related data
+// in form of key and value pair
+
+const person = {
+  firstName: 'Joaquin',
+  lastName: 'Phoenix',
+  age: 48,
+  car: {
+    brand: 'Toyota',
+    year: 2015,
+    color: 'red',
+  },
+};
+console.log(person);
