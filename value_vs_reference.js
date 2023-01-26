73749f4299785d785ab82f69611c3dd432cbe5b9
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-26 23:16:07 +0530

    shallow cloning

--- value_vs_reference.js
+++ value_vs_reference.js
@@ -42,3 +42,38 @@ console.log(individual === otherIndividual); // false
 const individual2 = individual;
 console.log(individual === individual2); // true
 // both point to same memory location
+
+// cloning arrays
+
+// spread operator (...)
+const numbers = [1, 2, 3, 4, 5];
+console.log(...numbers); // 1 2 3 4 5
+
+const newNumbers = [...numbers]; // shallow clone/copy
+newNumbers.push(6);
+
+console.log(newNumbers); // [1, 2, 3, 4, 5, 6]
+console.log(numbers === newNumbers); // false
+// both point to different locations
+
+// slice()
+const numbers2 = numbers.slice();
+console.log(numbers2); // 1, 2, 3, 4, 5]
+
+// cloning objects
+
+// spread operator
+const man = { name: 'John', age: 20 };
+const otherMan = { ...man }; // shallow copy
+
+man.name = 'David';
+console.log(man); // {name: 'David', age: 20}
+console.log(otherMan); // {name: 'John', age: 20}
+
+// Object.assign()
+const woman = { name: 'Jenny', age: 25 };
+const otherWoman = Object.assign({}, woman);
+
+woman.age = 22;
+console.log(woman); // {name: 'Jenny', age: 22}
+console.log(otherWoman); // {name: 'Jenny', age: 25}
