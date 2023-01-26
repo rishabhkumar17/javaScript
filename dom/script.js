50759398b88306c338aaa880c8e8b3aba80b9a88
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-27 03:19:54 +0530

    working with html classes - javascript update

--- dom/script.js
+++ dom/script.js
@@ -1,8 +1,8 @@
 const menuClicked = (currEle) => {
-  const menuItems = document.getElementsByClassName('menu-item');
-
-  for (let i = 0; i < menuItems.length; i++) {
-    menuItems[i].classList.remove('active');
-  }
+  // const menuItems = document.getElementsByClassName('menu-item');
+  document.querySelector('.active').classList.remove('active');
+  // for (let i = 0; i < menuItems.length; i++) {
+  //   menuItems[i].classList.remove('active');
+  // }
   currEle.classList.add('active');
 };
