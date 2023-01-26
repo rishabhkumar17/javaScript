a73f6a7a232a7757d230ba4fa12ea9020c7c12d5
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-27 04:12:50 +0530

    creating traversing removing nodes

--- dom/script.js
+++ dom/script.js
@@ -6,3 +6,35 @@ const menuClicked = (currEle) => {
   // }
   currEle.classList.add('active');
 };
+
+const subjects = document.querySelector('.subjects');
+subjects;
+{
+  /* <ul class=​"subjects">​…​</ul>​<li>​…​</li>​<li class=​"fav-subject">​…​</li>​<li>​…​</li>​</ul>​ */
+}
+subjects.firstElementChild;
+{
+  /* <li>​…​</li>​::marker​"Maths"</li>​ */
+}
+subjects.lastElementChild;
+{
+  /* <li>​…​</li>​::marker​"English"</li>​ */
+}
+const favSubject = document.querySelector('.fav-subject');
+favSubject;
+{
+  /* <li class=​"fav-subject">​…​</li>​::marker​"Science"</li>​ */
+}
+favSubject.previousElementSibling;
+{
+  /* <li>​…​</li>​::marker​"Maths"</li>​ */
+}
+favSubject.nextElementSibling;
+{
+  /* <li>​…​</li>​::marker​"English"</li>​ */
+}
+favSubject.parentElement;
+{
+  /* <ul class=​"subjects">​<li>​…​</li>​<li class=​"fav-subject">​…​</li>​<li>​…​</li>​</ul>​ */
+}
+favSubject.remove();
